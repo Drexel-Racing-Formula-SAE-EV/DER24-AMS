3a8674af082b07914a4117623e168db9c6b2d650
cole <cab572@drexel.edu>
2024-04-06 15:54:18 -0400

    added temp and votlage to canbus transmission

--- Core/Src/tasks/canbus_task.c
+++ Core/Src/tasks/canbus_task.c
@@ -67,6 +67,18 @@ void canbus_task_fn(void *arg)
     	ret = HAL_CAN_AddTxMessage(canbus->hcan, tx_header, can_data, &canbus->tx_mailbox);
     	data->canbus_fault = ret;
 
+    	packet = 2;
+		can_data[0] = TO_MSB16(packet);
+		can_data[1] = TO_LSB16(packet);
+		can_data[2] = TO_MSB16((int16_t)(data->max_temp * 10.0));
+		can_data[3] = TO_LSB16((int16_t)(data->max_temp * 10.0));
+		can_data[4] = TO_MSB16((int16_t)(data->min_voltage * 10.0));
+		can_data[5] = TO_LSB16((int16_t)(data->min_voltage * 10.0));
+		can_data[6] = TO_MSB16((int16_t)(data->max_voltage * 10.0));
+		can_data[7] = TO_LSB16((int16_t)(data->max_voltage * 10.0));
+		ret = HAL_CAN_AddTxMessage(canbus->hcan, tx_header, can_data, &canbus->tx_mailbox);
+		data->canbus_fault = ret;
+
     	// TODO: write out all the other packets!
         osDelayUntil(entry + (1000 / CAN_FREQ));
 
